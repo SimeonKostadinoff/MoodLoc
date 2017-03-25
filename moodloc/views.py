4f13d32d6afc593c0ff2b854602d0d69746bd99e
Boyan Bonev <boyan.i.bonev@gmail.com>
2017-03-25 17:53:41 +0000

    Added tweeter API.

--- moodloc/views.py
+++ moodloc/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+import tweepy
 
 # Create your views here.
 
+def tweets(request):
+        '''Dsiplay all tweets'''
+
+
 def base_page(request):
+        '''Tokens go here'''
+        auth = tweepy.OAuthHandler('', '')
+        auth.set_access_token('', '')
+
+        api = tweepy.API(auth)
+
+        for tweet in tweepy.Cursor(api.search, geocode='51.5073,-0.1277,50km').items(20):
+                print(tweet.text)
+
         return render(request, 'index.html') # render home page
