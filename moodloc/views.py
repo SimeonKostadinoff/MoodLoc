8cec8bc2d1eea519feefa1d072a0a11c378c6545
Boyan Bonev <boyan.i.bonev@gmail.com>
2017-03-26 02:44:35 +0100

    Added google maps with React.

--- moodloc/views.py
+++ moodloc/views.py
@@ -6,6 +6,10 @@ import json, requests
 
 def tweets(request, location):
         #Tokens go here
+        url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyC8ljIetSrO_QgEa-wJDqLHaIbEB0PRJ2U"
+        auth = tweepy.OAuthHandler('s50kOqoGiWSKWWM2ivCz7rcOe', 'N4SAbdjVIHMZWcWT6HPaysyKX9rN5tjKyPoC6IID49NdiDlS4d')
+        auth.set_access_token('338335567-3mqDJ2s2Gz0D9wFd52sLTcpD2hfCo27IVMSn8He8',
+                              'SbgR8QCBMVuIXQ7C9XKmbDfgd4cnvwMI9rP3OTyDwo7ZK')
 
         resp = requests.get(url=url)
         print(resp.content)
