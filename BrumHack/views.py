500a64c0886b037aad783146dc1f22db0336e684
AkbarCS <psyam12@nottingham.ac.uk>
2017-03-26 03:50:07 +0100

    Changes made to textAnalysis file to allow data to be normalised

--- BrumHack/views.py
+++ BrumHack/views.py
@@ -6,7 +6,7 @@ import tweepy
 def tweets(request):
     '''Dsiplay all tweets'''
 
-def base_page():
+def base_page(radius):
 	'''Tokens go here'''
 	auth = tweepy.OAuthHandler('s50kOqoGiWSKWWM2ivCz7rcOe', 'N4SAbdjVIHMZWcWT6HPaysyKX9rN5tjKyPoC6IID49NdiDlS4d')
 	auth.set_access_token('338335567-3mqDJ2s2Gz0D9wFd52sLTcpD2hfCo27IVMSn8He8', 'SbgR8QCBMVuIXQ7C9XKmbDfgd4cnvwMI9rP3OTyDwo7ZK')
@@ -15,15 +15,17 @@ def base_page():
 
 	tweetArray = []
 	
-	for tweet in tweepy.Cursor(api.search, geocode='51.5073,-0.1277,50km').items(20):
+	for tweet in tweepy.Cursor(api.search, geocode='51.5073,-0.1277,' + str(radius) + "km").items(100):
 			tweet = tweet.text.encode('utf-8', 'ignore')
-			#print(tweet)
 			tweetArray.append(tweet)
+	
+	#tweetString = ' '.join(tweetArray)
 		
 	return tweetArray
+	#return tweetString
 	
 	#return render(request, 'index.html') # render home page
 
 if __name__ == "__main__":
 	#request = 1
-	base_page()
+	base_page(5)
